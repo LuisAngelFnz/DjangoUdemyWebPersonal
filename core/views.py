500b2b315c255c9c9a13266641c050dd41229dcd
Luis Angel Fernandez Vasquez <luisangel.fnz@gmail.com>
2023-10-30 13:48:13 -0600

    Resolver practica de vistas con render

--- core/views.py
+++ core/views.py
@@ -1,39 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = '''
-<h1>Web Personal<h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Porta Folio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-</ul>
-'''
-
 
 def home(request):
-    return HttpResponse(html_base + '''
-        <h2>Portada</h2>
-        <p>Esto es la portada</p>
-    ''')
+    return render(request, 'core/home.html')
 
 
 def about(request):
-    return HttpResponse(html_base + '''
-        <h2>Acerca de</h2>
-        <p>Me llamo Luis</p>
-    ''')
+    return render(request, 'core/about.html')
 
 
 def portfolio(request):
-    return HttpResponse(html_base + '''
-        <h2>Portafolio</h2>
-        <p>Contenido del portafolio</p>
-    ''')
+    return render(request, 'core/portfolio.html')
 
 
 def contact(request):
-    return HttpResponse(html_base + '''
-        <h2>Contacto</h2>
-        <p>Contenido de contacto</p>
-    ''')
+    return render(request, 'core/contact.html')
