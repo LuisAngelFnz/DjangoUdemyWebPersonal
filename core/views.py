40883f645263a7cf09b906f5c81f5a37b1a4a21e
Luis Angel Fernandez Vasquez <luisangel.fnz@gmail.com>
2023-10-30 13:27:43 -0600

    Realizar practica de PortaFolio y Contacto

--- core/views.py
+++ core/views.py
@@ -5,6 +5,8 @@ html_base = '''
 <ul>
     <li><a href="/">Portada</a></li>
     <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portfolio/">Porta Folio</a></li>
+    <li><a href="/contact/">Contacto</a></li>
 </ul>
 '''
 
@@ -20,4 +22,18 @@ def about(request):
     return HttpResponse(html_base + '''
         <h2>Acerca de</h2>
         <p>Me llamo Luis</p>
+    ''')
+
+
+def portfolio(request):
+    return HttpResponse(html_base + '''
+        <h2>Portafolio</h2>
+        <p>Contenido del portafolio</p>
+    ''')
+
+
+def contact(request):
+    return HttpResponse(html_base + '''
+        <h2>Contacto</h2>
+        <p>Contenido de contacto</p>
     ''')
