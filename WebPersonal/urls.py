6bb8ee63e37e9a34ad77856691909abf03ccd2af
Luis Angel <luisangel.fnz@gmail.com>
2024-01-26 00:36:21 -0600

    Modificaciones para administración del app PortFolio
    Se realizaron traducciones de labels y visualización de campos
    DateTimeFIeld e ImageFIeld, visualizar imagenes en modo desarrollo

--- WebPersonal/urls.py
+++ WebPersonal/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from core import views
 
 urlpatterns = [
@@ -25,3 +26,9 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
