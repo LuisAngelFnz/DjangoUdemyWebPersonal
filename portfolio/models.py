f5eded6538ab508a05e9ce25bd750d39aea0137e
Luis Angel <luisangel.fnz@gmail.com>
2024-01-28 16:33:44 -0600

    Agregar atributo url al modelo Project
    Se agrego un nuevo atributo del modelo Project para mostrar
    en descripción un link para obtener más información

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title       = models.CharField(max_length=200, verbose_name='Titulo')
+    title       = models.CharField(verbose_name='Titulo',max_length=200)
     description = models.TextField(verbose_name='Descripción')
-    image       = models.ImageField(
-        verbose_name='Imagen', upload_to='projects'
-    )
-    created     = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
-    updated     = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
+    image       = models.ImageField(verbose_name='Imagen', upload_to='projects')
+    url         = models.URLField(verbose_name='Dirección Web', null=True, blank=True)
+    created     = models.DateTimeField(verbose_name='Fecha de creación', auto_now_add=True)
+    updated     = models.DateTimeField(verbose_name='Fecha de edición', auto_now=True)
 
     class Meta:
         verbose_name        = 'proyecto'
